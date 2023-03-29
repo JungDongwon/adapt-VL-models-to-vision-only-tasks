5fd7f7aad5b52db406f0fb898c11832cfb80a035
Nish <ntharani@usc.edu>
2023-03-29 02:05:24 +0000

    VisualBert results

--- Nish/setup.sh
+++ Nish/setup.sh
@@ -3,9 +3,9 @@ apt-get install python3.10-dev libgl1-mesa-glx unzip python3-setuptools vim --ye
 pip install transformers opencv-python scikit-learn datasets ipdb --quiet
 python -m pip install --upgrade pip
 python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
-if [ ! -d "tiny-imagenet-200" ]
-then
-    wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
-    unzip tiny-imagenet-200.zip
-    rm tiny-imagenet-200.zip
-fi
+# if [ ! -d "tiny-imagenet-200" ]
+# then
+#     wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
+#     unzip tiny-imagenet-200.zip
+#     rm tiny-imagenet-200.zip
+# fi
