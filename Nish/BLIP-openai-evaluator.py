c438727c2c35ce54b91338e4d9d090b2bcbdaf8b
Nish <ntharani@usc.edu>
2023-05-04 07:38:43 +0000

    Include final BLIP translated results

--- Nish/BLIP-openai-evaluator.py
+++ Nish/BLIP-openai-evaluator.py
@@ -1,13 +1,13 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from sklearn.model_selection import train_test_split
-from tqdm.notebook import tqdm
-from sklearn.metrics import accuracy_score
-import numpy as np
-from datasets import load_dataset, Image
-from transformers import BlipForQuestionAnswering, BlipProcessor, BlipConfig, BlipModel
+# import torch
+# import torch.nn as nn
+# import torch.optim as optim
+# from torch.utils.data import Dataset, DataLoader
+# from sklearn.model_selection import train_test_split
+# from tqdm.notebook import tqdm
+# from sklearn.metrics import accuracy_score
+# import numpy as np
+# from datasets import load_dataset, Image
+# from transformers import BlipForQuestionAnswering, BlipProcessor, BlipConfig, BlipModel
 import os
 import logging
 import argparse
@@ -18,82 +18,11 @@ import time
 
 logger = logging.getLogger(__name__)
 
-class ImageDataset(torch.utils.data.Dataset):
-
-    def __init__(self, config, image_files, text, processor, num_labels):
-        self.config = config
-        self.image_files = image_files
-        self.text = text
-        self.processor = processor
-        self.num_labels = num_labels
-
-    def __len__(self):
-        return len(self.image_files)
-
-    def __getitem__(self, idx):
-        text = self.text
-        image = self.image_files[idx][image_name]
-        label = self.image_files[idx][label_name]
-        if image.mode != "RGB":
-            image = image.convert("RGB")
-        encoding = self.processor(image, text, return_tensors="pt")
-        label_encoding = self.processor(text=self.config.id2label[str(label)], padding="max_length", return_tensors="pt").input_ids
-        label_encoding = label_encoding.squeeze()
-
-        # remove batch dimension
-        for k,v in encoding.items():
-            encoding[k] = v.squeeze()
-        encoding["labels"] = label_encoding
-        return encoding
-    
-def collate_fn(batch):
-    input_ids = [item['input_ids'] for item in batch]
-    pixel_values = [item['pixel_values'] for item in batch]
-    attention_mask = [item['attention_mask'] for item in batch]
-    labels = [item['labels'] for item in batch]
-    # create new batch
-    batch = {}
-    batch['input_ids'] = torch.stack(input_ids)
-    batch['attention_mask'] = torch.stack(attention_mask)
-    batch['pixel_values'] = torch.stack(pixel_values)
-    batch['labels'] = torch.stack(labels)
-    return batch
-
-@torch.no_grad()
-def evaluate(blip_model, device, processor, test_dataloader):
-    #losses = []  # List of scalar tensors
-    total_acc = 0.0
-    for batch in tqdm(test_dataloader):
-        batch = {k:v.to(device) for k,v in batch.items()}
-        outputs = blip_model.generate(**batch)  
-        outputs = outputs[:,0:max_label_token_length]
-        labels = batch['labels'][:,0:max_label_token_length]
-        outputs = processor.batch_decode(outputs, skip_special_tokens=True)
-        labels = processor.batch_decode(labels, skip_special_tokens=True)
-        #print('outputs:', outputs)
-        #print('labels:', labels)
-        correct = 0
-        for i in range(len(labels)):
-            output = outputs[i].replace(" ", "")
-            label = labels[i]
-            if output == label:
-                correct += 1
-        acc = (correct * 100) / len(labels)
-        total_acc += acc
-        #print(acc)
-
-    #stacked_losses = torch.stack(losses)  # (num_batches, ) 
-    #total_avg_loss = stacked_losses.mean()  # (num test examples, ) -> scalar
-    total_avg_acc = total_acc / len(test_dataloader)
-    #logger.info(f"Correct: {correct} / Total: {total}")
-    #logger.info(f"Average val loss: {total_avg_loss.item()}")
-    logger.info(f"Average val acc: {total_avg_acc}")
-    return total_avg_acc
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--outputs_file", default="./BLIP_Results/BLIP_cifar100_no_text_outputs.txt")
-    parser.add_argument("--labels_file", default="./BLIP_Results/BLIP_cifar100_no_text_labels.txt")
+    parser.add_argument("--outputs_file", default="./BLIP_Final_Results/BLIP_cifar100_no_text_outputs.txt")
+    parser.add_argument("--labels_file", default="./BLIP_Final_Results/BLIP_cifar100_no_text_labels.txt")
     parser.add_argument("--log_dir", default="./logs/")
     parser.add_argument("--evaluator_batch_size", default=10, type=int)
     
@@ -404,19 +333,12 @@ Input: \"""
         with open(translation_fname, 'a') as f:
             f.writelines(translation_lines)
         print(f"Running acc: {(correct/total):.2f}")
-        
-        
-       
-    # Evaluate labels against translated_outputs
-    n = len(labels)
-    correct = 0
-    for i in range(n):
-        if translated_outputs[i] == labels[i]:
-            correct += 1
     
-    acc = correct / n
+    acc = correct / total
     print(acc)
     
+    with open(translation_fname, 'a') as f:
+        f.write("Final accuracy: " + str(acc) + "\n")
     
     
     
